f2d80aefdba7e8ea41068f8feb92ab6a0b9d536a
Max Hutchinson <maxhutch@gmail.com>
2016-11-28 16:14:59 -0600

    Adding feature importance and fixing a bug in the splits.

--- src/test/scala/io/citrine/lolo/SimpleRegressionTreeTest.scala
+++ src/test/scala/io/citrine/lolo/SimpleRegressionTreeTest.scala
@@ -20,10 +20,32 @@ class SimpleRegressionTreeTest {
     }
   }
 
+  @Test
+  def longerTest(): Unit = {
+    val csv = TestUtils.readCsvAsDoubles("large_example.csv")
+    val trainingData = csv.map(vec => (vec.init, vec.last))
+    val DTLearner = new SimpleRegressionTreeLearner()
+    val start = System.nanoTime()
+    val DT = DTLearner.train(trainingData)
+    val duration = (System.nanoTime() - start) / 1.0e9
+
+    println(s"Training large case took ${duration} s")
+
+    /* We should be able to memorize the inputs */
+    trainingData.foreach { case (x, y) =>
+        assert(y == DT.predict(x))
+    }
+
+    /* The first feature should be the most important */
+    val importances = DT.getFeatureImportance()
+    assert(importances(0) == importances.max)
+  }
+
 }
 
 object SimpleRegressionTreeTest {
   def main(argv: Array[String]): Unit = {
-    new SimpleRegressionTreeTest().testSimpleTree()
+    // new SimpleRegressionTreeTest().testSimpleTree()
+    new SimpleRegressionTreeTest().longerTest()
   }
 }
