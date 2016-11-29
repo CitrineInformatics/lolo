9a384baf6a6bd573a4c7c9cc77709be5ac1dfce3
Max Hutchinson <maxhutch@gmail.com>
2016-11-29 12:32:19 -0600

    Adding test with categorical values; bugfix.

--- src/test/scala/io/citrine/lolo/RegressionTreeTest.scala
+++ src/test/scala/io/citrine/lolo/RegressionTreeTest.scala
@@ -45,12 +45,35 @@ class RegressionTreeTest {
     assert(importances(0) == importances.max)
     */
   }
+  @Test
+  def testCategorical(): Unit = {
+    val csv = TestUtils.readCsv("large_example_with_cat.csv")
+    val trainingData = csv.map(vec => (vec.init, vec.last.asInstanceOf[Double]))
+    val DTLearner = new RegressionTreeLearner()
+    val N = 100
+    val start = System.nanoTime()
+    val DT = DTLearner.train(trainingData)
+    (0 until N).map(i => DTLearner.train(trainingData))
+    val duration = (System.nanoTime() - start) / 1.0e9
 
+    println(s"Training large case took ${duration/N} s")
+
+    /* We should be able to memorize the inputs */
+    trainingData.foreach { case (x, y) =>
+        assert(y == DT.predict(x))
+    }
+
+    /* The first feature should be the most important
+    val importances = DT.getFeatureImportance()
+    println(importances.toList)
+    assert(importances(0) == importances.max)
+    */
+  }
 }
 
 object RegressionTreeTest {
   def main(argv: Array[String]): Unit = {
     // new SimpleRegressionTreeTest().testSimpleTree()
-    new RegressionTreeTest().longerTest()
+    new RegressionTreeTest().testCategorical()
   }
 }
