2c275dc6594a8f047671a15fdd12fc806de6d235
Max Hutchinson <maxhutch@gmail.com>
2016-12-01 19:35:17 -0600

    Tightening the scala and docs.

--- src/main/scala/io/citrine/lolo/encoders/CategoricalEncoder.scala
+++ src/main/scala/io/citrine/lolo/encoders/CategoricalEncoder.scala
@@ -1,32 +1,41 @@
 package io.citrine.lolo.encoders
 
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-
 /**
+  * Encoder that maps a categorical variable to a char
   * Created by maxhutch on 11/28/16.
+  *
+  * @param encoding underlying map to use
+  * @tparam T type of the categorical variable
   */
 class CategoricalEncoder[T](encoding: Map[T, Char]) {
 
+  /** Inverse of the encoding */
   lazy val decoding: Map[Char, T] = encoding.groupBy(_._2).mapValues(_.keys.head)
 
+  /**
+    * Just call the encoding.  Use 0 for unknown inputs
+    * @param input to encode
+    * @return encoded input as a char
+    */
   def encode(input: T): Char = encoding.getOrElse(input, 0)
 
+  /**
+    * Just call the decoding.
+    * @param output to decode
+    * @return decoded output
+    */
   def decode(output: Char): T = decoding(output)
 }
 
+/** Companion object */
 object CategoricalEncoder {
+  /**
+    * Build an encoder from a list of input values
+    * @param values to encode
+    * @tparam T type of the encoder
+    * @return an encoder for those inputs
+    */
   def buildEncoder[T](values: Seq[T]): CategoricalEncoder[T] = {
-    var index: Char = 1 // leave 0 for unknown
-    val pairs = ListBuffer.empty[(T, Char)]
-    val seen = new mutable.HashSet[T]()
-    for (v <- values) {
-      if (!seen.contains(v)) {
-        seen.add(v)
-        pairs.append((v, index))
-        index = (index + 1).toChar
-      }
-    }
-    new CategoricalEncoder[T](pairs.toMap)
+    new CategoricalEncoder[T](values.distinct.zipWithIndex.map(p => (p._1, (p._2 + 1).toChar)).toMap)
   }
 }
