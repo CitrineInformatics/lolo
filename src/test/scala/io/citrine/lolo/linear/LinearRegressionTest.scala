c475851f2f7b6e3ce61e9965c1678d9bae542e28
Max Hutchinson <maxhutch@gmail.com>
2016-12-06 09:14:44 -0600

    Adding weighted regression.

--- src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
+++ src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
@@ -10,12 +10,12 @@ import scala.util.Random
   */
 @Test
 class LinearRegressionTest {
+  val n = 5
+  val k = 4
+  val data = DenseMatrix.rand[Double](n,k)
+  val beta0 = DenseVector.rand[Double](k)
 
   def testRegressionNoIntercept(): Unit = {
-    val n = 5
-    val k = 4
-    val data = DenseMatrix.rand[Double](n,k)
-    val beta0 = DenseVector.rand[Double](k)
     val result = data * beta0
 
     val trainingData = (0 until n).map{ i =>
@@ -38,10 +38,6 @@ class LinearRegressionTest {
   }
 
   def testRegression(): Unit = {
-    val n = 5
-    val k = 4
-    val data = DenseMatrix.rand[Double](n,k)
-    val beta0 = DenseVector.rand[Double](k)
     val int0 = Random.nextDouble()
     val result = data * beta0 + int0
 
@@ -62,6 +58,29 @@ class LinearRegressionTest {
       println(s"Predicted vs Actual: ${p}, ${a}")
       assert(Math.abs(p - a) < 1.0e-9)
     }
+  }
+
+  def testWeightedRegression(): Unit = {
+    val int0 = Random.nextDouble()
+    val result = data * beta0 + int0
+
+    val trainingData = (0 until n).map{ i =>
+      (data.t(::, i).toDenseVector.toArray.toVector, result(i))
+    }
+
+    val lr = new LinearRegressionLearner()
+    val lrm = lr.train(trainingData, weights = Some(Seq.tabulate(n)(i => Math.abs(Random.nextDouble()))))
+    val model = lrm.getModel()
+    val output = model.transform(trainingData.map(_._1))
+    val predicted = output.getExpected()
+
+    val beta = output.getGradient().head
+    println(beta, beta0.toArray.toVector)
+
+    predicted.zip(result.toArray).foreach{ case (p, a) =>
+      println(s"Predicted vs Actual: ${p}, ${a}")
+      assert(Math.abs(p - a) < 1.0e-9)
+    }
 
   }
 }
@@ -70,5 +89,6 @@ object LinearRegressionTest {
   def main(argv: Array[String]): Unit = {
     new LinearRegressionTest().testRegressionNoIntercept()
     new LinearRegressionTest().testRegression()
+    new LinearRegressionTest().testWeightedRegression()
   }
 }
