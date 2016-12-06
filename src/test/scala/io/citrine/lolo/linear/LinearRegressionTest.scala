7366580447e36621d1871cdc4dee7c97c36fba0a
Max Hutchinson <maxhutch@gmail.com>
2016-12-06 08:45:38 -0600

    Adding intercept option.

--- src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
+++ src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
@@ -3,6 +3,8 @@ package io.citrine.lolo.linear
 import breeze.linalg.{DenseMatrix, DenseVector}
 import org.junit.Test
 
+import scala.util.Random
+
 /**
   * Created by maxhutch on 12/6/16.
   */
@@ -15,6 +17,33 @@ class LinearRegressionTest {
     val data = DenseMatrix.rand[Double](n,k)
     val beta0 = DenseVector.rand[Double](k)
     val result = data * beta0
+
+    val trainingData = (0 until n).map{ i =>
+      (data.t(::, i).toDenseVector.toArray.toVector, result(i))
+    }
+
+    val lr = new LinearRegressionLearner(intercept = false)
+    val lrm = lr.train(trainingData)
+    val model = lrm.getModel()
+    val output = model.transform(trainingData.map(_._1))
+    val predicted = output.getExpected()
+
+    val beta = output.getGradient().head
+    println(beta, beta0.toArray.toVector)
+
+    predicted.zip(result.toArray).foreach{ case (p, a) =>
+      println(s"Predicted vs Actual: ${p}, ${a}")
+      assert(Math.abs(p - a) < 1.0e-9)
+    }
+  }
+
+  def testRegression(): Unit = {
+    val n = 5
+    val k = 4
+    val data = DenseMatrix.rand[Double](n,k)
+    val beta0 = DenseVector.rand[Double](k)
+    val int0 = Random.nextDouble()
+    val result = data * beta0 + int0
 
     val trainingData = (0 until n).map{ i =>
       (data.t(::, i).toDenseVector.toArray.toVector, result(i))
@@ -40,5 +69,6 @@ class LinearRegressionTest {
 object LinearRegressionTest {
   def main(argv: Array[String]): Unit = {
     new LinearRegressionTest().testRegressionNoIntercept()
+    new LinearRegressionTest().testRegression()
   }
 }
