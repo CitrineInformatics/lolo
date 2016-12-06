0640512ab9dc7b29c97630b8c0857f782d08818b
Max Hutchinson <maxhutch@gmail.com>
2016-12-06 13:35:43 -0600

    Protecting linear regression from categorical inputs; they are ignored.

--- src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
+++ src/test/scala/io/citrine/lolo/linear/LinearRegressionTest.scala
@@ -22,6 +22,7 @@ class LinearRegressionTest {
   /**
     * Test regression without an intercept or weight; this is easiest
     */
+  @Test
   def testRegressionNoIntercept(): Unit = {
     val result = data * beta0
 
@@ -43,6 +44,7 @@ class LinearRegressionTest {
   /**
     * Add an intercept
     */
+  @Test
   def testRegression(): Unit = {
     val int0 = Random.nextDouble()
     val result = data * beta0 + int0
@@ -65,6 +67,7 @@ class LinearRegressionTest {
   /**
     * Add an intercept and random weights
     */
+  @Test
   def testWeightedRegression(): Unit = {
     val int0 = Random.nextDouble()
     val result = data * beta0 + int0
@@ -87,6 +90,7 @@ class LinearRegressionTest {
   /**
     * Test when there are more features than training rows
     */
+  @Test
   def testUnderconstrained(): Unit = {
     /** Number of training rows */
     val n = 4
@@ -112,6 +116,31 @@ class LinearRegressionTest {
     assert(norm(new DenseVector(predicted.toArray) - result) < 1.0e-9, "Predictions are inaccurate")
   }
 
+  /**
+   * Add an intercept
+   */
+  @Test
+  def testCategoricalValue(): Unit = {
+    val int0 = Random.nextDouble()
+    val result = data * beta0 + int0
+
+    val trainingData = (0 until n).map{ i =>
+      ("Foo" +: data.t(::, i).toDenseVector.toArray.toVector.asInstanceOf[Vector[Any]] :+ "Bar" , result(i))
+    }
+
+    val lr = new LinearRegressionLearner()
+    val lrm = lr.train(trainingData)
+    val model = lrm.getModel()
+    val output = model.transform(trainingData.map(_._1))
+    val predicted = output.getExpected()
+    val beta = output.getGradient().head
+
+    assert(norm(new DenseVector(beta.toArray)(1 to -2) - beta0) < 1.0e-9, "Coefficients are inaccurate")
+    assert(beta.head == 0.0, "Non-zero graident given to leading categorical feature")
+    assert(beta.last == 0.0, "Non-zero graident given to trailing categorical feature")
+    assert(norm(new DenseVector(predicted.toArray) - result) < 1.0e-9, "Predictions are inaccurate")
+  }
+
 }
 
 /** Companion driver */
@@ -125,5 +154,6 @@ object LinearRegressionTest {
     new LinearRegressionTest().testRegression()
     new LinearRegressionTest().testWeightedRegression()
     new LinearRegressionTest().testUnderconstrained()
+    new LinearRegressionTest().testCategoricalValue()
   }
 }
