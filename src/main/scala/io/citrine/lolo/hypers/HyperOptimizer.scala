92ef549bb9d32e349b10f3b2283e3817f9463be8
Max Hutchinson <maxhutch@gmail.com>
2016-12-08 06:48:02 -0600

    Adding simple hyperparameter optimization schemes

---
+++ src/main/scala/io/citrine/lolo/hypers/HyperOptimizer.scala
@@ -0,0 +1,38 @@
+package io.citrine.lolo.hypers
+
+import io.citrine.lolo.Learner
+
+/**
+  * Base class for hyperparameter optimizers
+  *
+  * They take a range of hypers as a Map[(String, Seq[Any])] and output the best map and loss
+  * Created by maxhutch on 12/8/16.
+  *
+  * @param base learner to optimize parameters for
+  */
+abstract class HyperOptimizer(base: Learner) {
+
+  /**
+    * Add a 1D hyper range to the space searched by this optimizer
+    *
+    * @param name   of the hyper
+    * @param values it takes, enumerated as a seq
+    * @return calling instance
+    */
+  def addHyperGrid(name: String, values: Seq[Any]): this.type = {
+    hyperGrids = hyperGrids + ((name, values))
+    this
+  }
+
+  /** The search space */
+  var hyperGrids: Map[String, Seq[Any]] = Map()
+
+  /**
+    * Optimize the hypers over the defined search space
+    *
+    * @param trainingData  the data to train/test on
+    * @param numIterations to take before terminating
+    * @return the best hyper map found in give iterations and the corresponding loss
+    */
+  abstract def optimize(trainingData: Seq[(Vector[Any], Any)], numIterations: Int = 8): (Map[String, Any], Double)
+}
