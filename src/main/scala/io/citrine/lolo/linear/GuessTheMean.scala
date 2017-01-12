7ecb9210cd0d0f4619d972ade2f9192dfbaa1055
Max Hutchinson <maxhutch@gmail.com>
2017-01-12 09:56:31 -0600

    Adding types to GTM results; adding mode

--- src/main/scala/io/citrine/lolo/linear/GuessTheMean.scala
+++ src/main/scala/io/citrine/lolo/linear/GuessTheMean.scala
@@ -17,15 +17,18 @@ class GuessTheMeanLearner extends Learner {
     * @return training result containing a model
     */
   override def train(trainingData: Seq[(Vector[Any], Any)], weights: Option[Seq[Double]]): TrainingResult = {
-    val data = trainingData.map(_._2.asInstanceOf[Double]).zip(weights.getOrElse(Seq.fill(trainingData.size)(1.0)))
-    val mean = data.map(p => p._1 * p._2).sum / data.map(_._2).sum
+    val data = trainingData.map(_._2).zip(weights.getOrElse(Seq.fill(trainingData.size)(1.0)))
+    val mean = data match {
+      case x: Seq[(Double, Double)] => x.map(p => p._1 * p._2).sum / data.map(_._2).sum
+      case x: Seq[(Any, Double)] => x.groupBy(_._1).mapValues(_.map(_._2).sum).maxBy(_._2)._1
+    }
 
     new GuessTheMeanTrainingResult(new GuessTheMeanModel(mean))
   }
 }
 
 @SerialVersionUID(999L)
-class GuessTheMeanTrainingResult(model: GuessTheMeanModel) extends TrainingResult {
+class GuessTheMeanTrainingResult[T](model: GuessTheMeanModel[T]) extends TrainingResult {
   /**
     * Get the hyperparameters used to train this model
     *
@@ -33,22 +36,22 @@ class GuessTheMeanTrainingResult(model: GuessTheMeanModel) extends TrainingResul
     */
   override def getHypers(): Map[String, Any] = Map.empty[String, Any]
 
-  override def getModel(): Model[GuessTheMeanResult] = model
+  override def getModel(): Model[GuessTheMeanResult[T]] = model
 }
 
 @SerialVersionUID(1000L)
-class GuessTheMeanModel(mean: Any) extends Model[GuessTheMeanResult] {
+class GuessTheMeanModel[T](mean: T) extends Model[GuessTheMeanResult[T]] {
 
-  def transform(inputs: Seq[Vector[Any]]): GuessTheMeanResult = {
+  def transform(inputs: Seq[Vector[Any]]): GuessTheMeanResult[T] = {
     new GuessTheMeanResult(Seq.fill(inputs.size)(mean))
   }
 }
 
-class GuessTheMeanResult(result: Seq[Any]) extends PredictionResult[Any] {
+class GuessTheMeanResult[T](result: Seq[T]) extends PredictionResult[T] {
   /**
     * Get the expected values for this prediction
     *
     * @return expected value of each prediction
     */
-  override def getExpected(): Seq[Any] = result
+  override def getExpected(): Seq[T] = result
 }
