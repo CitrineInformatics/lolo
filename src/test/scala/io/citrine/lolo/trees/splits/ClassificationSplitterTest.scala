e4407237a8d6cfe1e2b7af3af0e00358f5d1dafa
Max Hutchinson <maxhutch@gmail.com>
2017-10-26 08:48:59 -0500

    Cleaning up the ClassificationSplitterTest

--- src/test/scala/io/citrine/lolo/trees/splits/ClassificationSplitterTest.scala
+++ src/test/scala/io/citrine/lolo/trees/splits/ClassificationSplitterTest.scala
@@ -3,14 +3,16 @@ package io.citrine.lolo.trees.splits
 import io.citrine.lolo.TestUtils
 import io.citrine.lolo.encoders.CategoricalEncoder
 import io.citrine.theta.Stopwatch
-import org.junit.Test
 
 
 /**
-  * Created by maxhutch on 12/1/16.
+  * Hold some performance profiling routines
   */
 class ClassificationSplitterTest {
 
+  /**
+    * Evaluate the split finding performance on large and small datasets
+    */
   def testSplitterPerformance(): Unit = {
     val timeLarge = Stopwatch.time({
       ClassificationSplitter.getBestSplit(ClassificationSplitterTest.encodedData, 12, 1)
@@ -18,28 +20,39 @@ class ClassificationSplitterTest {
     println(timeLarge)
 
     val timeSmall = Stopwatch.time({
-      (0 until 256).foreach { i =>
+      (0 until ClassificationSplitterTest.nRow / ClassificationSplitterTest.nSubset).foreach { i =>
         ClassificationSplitter.getBestSplit(ClassificationSplitterTest.subset, 12, 1)
       }
     })
     println(timeSmall)
   }
 
-
 }
 
+/**
+  * Setup the test data
+  */
 object ClassificationSplitterTest {
+  val nRow = 4096
+  val nLabel = 4096
+  val nSubset = 4
+
   val testData = TestUtils.binTrainingData(
-    TestUtils.generateTrainingData(1024, 12),
-    responseBins = Some(256)
+    TestUtils.generateTrainingData(nRow, 12),
+    responseBins = Some(nLabel)
   )
+
   val encoder = CategoricalEncoder.buildEncoder(testData.map(_._2))
   val encodedData = testData.map{case (f, l) =>
     (f.asInstanceOf[Vector[AnyVal]], encoder.encode(l), 1.0)
   }
 
-  val subset = encodedData.take(4)
+  val subset = encodedData.take(nSubset)
 
+  /**
+    * Run the tests
+    * @param args foo
+    */
   def main(args: Array[String]): Unit = {
     new ClassificationSplitterTest().testSplitterPerformance()
   }
