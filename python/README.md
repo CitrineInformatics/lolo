39272dab9ddbd88cdaed970079855dfa0b43f64f
Logan Ward <ward.logan.t@gmail.com>
2018-10-26 10:51:44 -0500

    Initial commit for lolopy

---
+++ python/README.md
@@ -0,0 +1,3 @@
+# Python Wrapper for Lolo
+
+This directory contains a Python wrapper for Lolo, which is currently under development
---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='lolopy',
+    version='0.0.1',
+    packages=['lolopy'],
+    description='Python wrapper for the Lolo machine learning library',
+)
