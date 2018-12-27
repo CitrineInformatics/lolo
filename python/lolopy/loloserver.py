b22c935573d5d9654a8a1eba3fd57a421a87bde2
Logan Ward <ward.logan.t@gmail.com>
2018-12-27 16:41:45 -0600

    Added ability to use a single JVM for all models
    Avoids spawning many JVMs by default

--- python/lolopy/loloserver.py
+++ python/lolopy/loloserver.py
@@ -4,6 +4,8 @@ from subprocess import STDOUT
 import sys
 import os
 
+# Used for allowing multiple objects to use the same gateway
+_lolopy_gateway = None
 
 # TODO: Auto-detect the version of lolo? [Requires lolo installation (not compatible with PyPi) and/or internet connection to check Maven, and causes a maintenance burden]
 _lolo_version = '1.0.3'
@@ -25,13 +27,17 @@ def get_java_gateway(reuse=True):
     """Get a JavaGateway with Lolo on the class path
 
     Args:
-        reuse (bool): Whether to reuse an already-existing gateway (TBD)
+        reuse (bool): Whether to reuse an already-existing gateway
     Returns:
         (JavaGateway) A launched JavaGateway instance
     """
-    lolo_path = find_lolo_jar()
-    assert os.path.isfile(lolo_path), 'Lolo jar not found'
 
-    _gateway = JavaGateway.launch_gateway(classpath=os.path.pathsep.join([
-        os.path.abspath(lolo_path)]), die_on_exit=True)
-    return _gateway
+    global _lolopy_gateway
+
+    if _lolopy_gateway is None or not reuse:
+        lolo_path = find_lolo_jar()
+        assert os.path.isfile(lolo_path), 'Lolo jar not found'
+
+        _lolopy_gateway = JavaGateway.launch_gateway(classpath=os.path.pathsep.join([
+            os.path.abspath(lolo_path)]), die_on_exit=True)
+    return _lolopy_gateway
