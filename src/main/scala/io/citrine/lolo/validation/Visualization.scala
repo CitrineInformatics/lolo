79a674c27a279f5b7f520dc49da84724221952d8
Max Hutchinson <maxhutch@gmail.com>
2019-01-18 11:11:02 -0600

    Make the Gaussian and Cauchy overlays configurable

--- src/main/scala/io/citrine/lolo/validation/Visualization.scala
+++ src/main/scala/io/citrine/lolo/validation/Visualization.scala
@@ -18,17 +18,28 @@ trait Visualization[T] {
 
 /**
   * Histogram of the error divided by the predicted uncertainty
+  *
+  * Gaussian and Cauchy fits are preformed via quantiles:
+  *  - standard deviation is taken as the 68th percentile standard error
+  *  - gamma is taken as the 50th percentile standard error
   * @param nBins number of bins in the histogram
   * @param range of the horizontal axis, e.g. x \in [-range/2, range/2]
+  * @param fitGaussian whether to fit and plot a Gaussian distribution
+  * @param fitCauchy whether to fit and plot a Cauchy distribution
   */
-case class StandardResidualHistogram(nBins: Int = 128, range: Double = 8.0) extends Visualization[Double] {
+case class StandardResidualHistogram(
+                                      nBins: Int = 128,
+                                      range: Double = 8.0,
+                                      fitGaussian: Boolean = true,
+                                      fitCauchy: Boolean = true
+                                    ) extends Visualization[Double] {
 
   override def visualize(data: Iterable[(PredictionResult[Double], Seq[Double])]): CategoryChart = {
     val pua: Seq[(Double, Double, Double)] = data.flatMap { case (predictions, actual) =>
       (predictions.getExpected(), predictions.getUncertainty().get.asInstanceOf[Seq[Double]], actual).zipped.toSeq
     }.toSeq
 
-    val standardErrors = pua.map { case (predicted, sigma, actual) => (predicted - actual) / sigma }
+    val standardErrors = pua.map { case (predicted, sigma, actual) => (predicted - actual) / sigma }.sorted
 
     val bins = (-nBins / 2 until nBins / 2).map { idx =>
       (range * idx.toDouble / nBins, range * (idx.toDouble + 1) / nBins)
@@ -40,13 +51,18 @@ case class StandardResidualHistogram(nBins: Int = 128, range: Double = 8.0) exte
 
     val chart: CategoryChart = new CategoryChartBuilder().build()
     chart.addSeries("data", counts.map(_._1).toArray, counts.map(_._2.toDouble).toArray)
-    val normalVar = 1.0
-    val normalSeries = counts.map(_._1).map(x => Math.exp(-x * x / (2 * normalVar)) / Math.sqrt(2 * Math.PI * normalVar))
-    chart.addSeries(f"sigma=${Math.sqrt(normalVar)}%6.3f", counts.map(_._1).toArray, normalSeries.toArray)
-    val gamma = 1.0
-    val cauchy1 = new CauchyDistribution(0.0, gamma)
-    val cauchySeries = counts.map(_._1).map(x => cauchy1.density(x))
-    chart.addSeries(f"gamma=${gamma}%6.3f", counts.map(_._1).toArray, cauchySeries.toArray)
+
+    if (fitGaussian) {
+      val normalVar = standardErrors.drop((standardErrors.size * 0.68).toInt).head
+      val normalSeries = counts.map(_._1).map(x => Math.exp(-x * x / (2 * normalVar)) / Math.sqrt(2 * Math.PI * normalVar))
+      chart.addSeries(f"Normal(0, ${Math.sqrt(normalVar)}%6.3f)", counts.map(_._1).toArray, normalSeries.toArray)
+    }
+    if (fitCauchy) {
+      val gamma = standardErrors.drop((standardErrors.size * 0.5).toInt).head
+      val cauchy1 = new CauchyDistribution(0.0, gamma)
+      val cauchySeries = counts.map(_._1).map(x => cauchy1.density(x))
+      chart.addSeries(f"Cauchy(0, ${gamma}%6.3f)", counts.map(_._1).toArray, cauchySeries.toArray)
+    }
 
     chart.setTitle("(predicted - actual) / (predicted uncertainty)")
     chart.setYAxisTitle("probability density")
