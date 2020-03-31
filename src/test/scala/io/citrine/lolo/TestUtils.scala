6498a00281769166bb0a1d7e2d95afa9329970d2
Gregor Robinson <grobinson@citrine.io>
2020-03-31 15:34:58 -0700

    WIP: Add random mocker.

--- src/test/scala/io/citrine/lolo/TestUtils.scala
+++ src/test/scala/io/citrine/lolo/TestUtils.scala
@@ -1,7 +1,12 @@
 package io.citrine.lolo
 
 import io.citrine.lolo.stats.functions.Friedman
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.MockitoSugar
 
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
 import scala.util.{Random, Try}
 
 /**
@@ -77,4 +82,60 @@ object TestUtils {
     outputData
   }
 
+}
+
+class RandomMocker(baseRng: Random) extends MockitoSugar {
+  val m = mock[Random]
+  val queue = new mutable.Queue[(String, AnyRef, Int, Seq[AnyRef])]
+
+  private def enqueueForVerification(methodName: String, returnValue: AnyRef, wantedNumberOfInvocations: Int, args: AnyRef*): Unit = {
+    queue.enqueue((methodName, returnValue, wantedNumberOfInvocations, args))
+  }
+
+  def getMock() = m
+
+  def verify(): Unit = {
+    val order = Mockito.inOrder(m)
+    queue.foreach { case (methodName, _, wantedNumberOfInvocations, args) =>
+      val argTypes = args.map(_.getClass)
+      val verification = order.verify(m, Mockito.times(wantedNumberOfInvocations))
+      val method = verification.getClass.getMethod(methodName, argTypes: _*)
+      method.invoke(verification, args: _*)
+    }
+    order.verifyNoMoreInteractions()
+  }
+
+  def shuffle[T, CC[X] <: TraversableOnce[X]](
+                  xs: CC[T],
+                  toReturn: Option[CC[T]] = None
+                 )(implicit bf: CanBuildFrom[CC[T], T, CC[T]]): CC[T] = {
+    val out = toReturn match {
+      case Some(shuffled) => shuffled
+      case None => baseRng.shuffle(xs)
+    }
+    enqueueForVerification(methodName = "shuffle", returnValue = out.asInstanceOf[AnyRef], wantedNumberOfInvocations = 1, args = xs.asInstanceOf[AnyRef])
+    doReturn(out).when(m).shuffle(xs)
+  }
+
+  def nextDouble(toReturn: Option[Double] = None): Double = {
+    val out = toReturn match {
+      case Some(x) => x
+      case None => baseRng.nextDouble()
+    }
+    enqueueForVerification(methodName = "nextDouble", returnValue = out.asInstanceOf[AnyRef], wantedNumberOfInvocations = 1, args = Seq())
+    doReturn(out).when(m).nextDouble()
+  }
+}
+
+class TestRandomMocker extends MockitoSugar {
+  @Test
+  def testShuffle(): Unit = {
+    def mocker = new RandomMocker(Random)
+    val itemsToShuffle = (1 to 10).toVector
+    val expected = mocker.shuffle(itemsToShuffle)
+    val actual = mocker.getMock().shuffle(itemsToShuffle)
+    expected.zip(actual).foreach { case (e,a) =>
+      assert(e == a, "RandomMocker.shuffle(...) did not return the same value as RandomMocker.getMock().shuffle(...)")
+    }
+  }
 }
