ce88bcb8b8fae5d504664f6a1db18fa9de7921a8
Sean Friedowitz <sfriedowitz@citrine.io>
2022-02-07 18:56:53 -0800

    remov efommil

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,22 +1,20 @@
 import sbt._
 
 object Dependencies {
-  lazy val netlibVersion = "1.1.2"
-  lazy val junitVersion = "4.13.1"
   lazy val thetaVersion = "1.1.4"
   lazy val breezeVersion = "2.0"
+  lazy val junitVersion = "4.13.1"
   lazy val scalaTestVersion = "3.2.2"
 
   // this is used in build.sbt so that it works with both 2.12 and 2.13
   lazy val scalaParallelCollectionsVersion = "1.0.0"
 
   val loloDeps = Seq(
-    "com.github.fommil.netlib" % "all"             % netlibVersion pomOnly (),
-    "junit"                    % "junit"           % junitVersion     % "test",
-    "org.scalanlp"            %% "breeze"          % breezeVersion,
-    "io.citrine"              %% "theta"           % thetaVersion,
-    "org.scalatest"           %% "scalatest"       % scalaTestVersion % "test",
-    "com.novocode"             % "junit-interface" % "0.11"           % "test",
-    "org.knowm.xchart"         % "xchart"          % "3.5.2"
+    "io.citrine"      %% "theta"           % thetaVersion,
+    "org.scalanlp"    %% "breeze"          % breezeVersion,
+    "junit"            % "junit"           % junitVersion     % "test",
+    "org.scalatest"   %% "scalatest"       % scalaTestVersion % "test",
+    "com.novocode"     % "junit-interface" % "0.11"           % "test",
+    "org.knowm.xchart" % "xchart"          % "3.5.2"
   )
 }
