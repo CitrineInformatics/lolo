1c126194c86fc2908a6da3897336f9864f78d0ad
Brendan Folie <bfolie@citrine.io>
2023-03-22 15:59:13 -0700

    use abstract class instead of trait

--- src/main/scala/io/citrine/lolo/bags/BaggedTrainingError.scala
+++ src/main/scala/io/citrine/lolo/bags/BaggedTrainingError.scala
@@ -1,6 +1,6 @@
 package io.citrine.lolo.bags
 
-sealed trait BaggedTrainingError extends Throwable {
+sealed abstract class BaggedTrainingError(msg: String) extends Exception(msg) {
   def numRows: Int
   def numRequired: Int
 }
@@ -8,15 +8,15 @@ sealed trait BaggedTrainingError extends Throwable {
 case class InsufficientTrainingDataException(
     numRows: Int,
     numRequired: Int
-) extends Exception(
+) extends BaggedTrainingError(
   s"Bagged training requires at least $numRequired rows, but only $numRows were provided."
-) with BaggedTrainingError
+)
 
 case class InsufficientOutputDataException(
     numRows: Int,
     numRequired: Int,
     index: Int
-) extends Exception(
+) extends BaggedTrainingError(
   s"Bagged multi-output training requires at least $numRequired rows for each output, " +
     s"but only $numRows were provided for output $index."
-) with BaggedTrainingError
+)
