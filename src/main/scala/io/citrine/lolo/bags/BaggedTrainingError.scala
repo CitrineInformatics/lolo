24d594d306ef072652eeeadb527c67274dc79953
Brendan Folie <bfolie@citrine.io>
2023-03-22 15:23:17 -0700

    create errors for not enough training data

---
+++ src/main/scala/io/citrine/lolo/bags/BaggedTrainingError.scala
@@ -0,0 +1,22 @@
+package io.citrine.lolo.bags
+
+sealed trait BaggedTrainingError {
+  def numRows: Int
+  def numRequired: Int
+}
+
+case class InsufficientTrainingDataException(
+    numRows: Int,
+    numRequired: Int
+) extends Exception(
+  s"Bagged training requires at least $numRequired rows, but only $numRows were provided."
+) with BaggedTrainingError
+
+case class InsufficientOutputDataException(
+    numRows: Int,
+    numRequired: Int,
+    index: Int
+) extends Exception(
+  s"Bagged multi-output training requires at least $numRequired rows for each output, " +
+    s"but only $numRows were provided for output $index."
+) with BaggedTrainingError
